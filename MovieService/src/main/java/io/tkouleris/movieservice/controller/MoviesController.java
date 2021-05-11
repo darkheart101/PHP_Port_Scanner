5c43c93d30130823a90d8cc5fc001b89eeb830e8
Thodoris Kouleris <kurokokoro@windowslive.com>
2021-05-11 06:32:01 +0300

    authentication

--- MovieService/src/main/java/io/tkouleris/movieservice/controller/MoviesController.java
+++ MovieService/src/main/java/io/tkouleris/movieservice/controller/MoviesController.java
@@ -9,14 +9,19 @@ import io.tkouleris.movieservice.dto.response.ApiResponse;
 import io.tkouleris.movieservice.dto.response.RatedMovie;
 import io.tkouleris.movieservice.entity.Movie;
 import io.tkouleris.movieservice.entity.Rating;
+import io.tkouleris.movieservice.entity.User;
 import io.tkouleris.movieservice.repository.IMovieRepository;
+import io.tkouleris.movieservice.service.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -32,32 +37,18 @@ public class MoviesController {
     @Autowired
     private IMovieRepository movieRepository;
 
+    @Autowired
+    private Authentication authentication;
+
     @GetMapping(path="/all", produces = "application/json")
 //    @HystrixCommand(fallbackMethod = "getFallbackAllMovies")
     public ResponseEntity<Object> getAll(){
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYyMDYxNzU1NSwiZXhwIjoxNjIwNjIxMTU1LCJuYmYiOjE2MjA2MTc1NTUsImp0aSI6IkRpRjIwTlE0OGhlbmx5QjQiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.YDO3hyZ9mNP7hr-ZQiQTPLJqb5GOxYGSHQmyf4wL1yU");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity entity = new HttpEntity(headers);
-        System.out.println("AUTHENTICATION FIRST==========================");
-        RestTemplate authRestTemplate = new RestTemplate();
 
-        try {
-            ResponseEntity<AuthResponse> response = authRestTemplate.exchange("http://127.0.0.1:8000/api/verify", HttpMethod.POST, entity,  AuthResponse.class);
-//            AuthResponse authResponse = response.getBody();
-            System.out.println(response.getBody().success);
-            System.out.println(response.getBody().user.id);
-            System.out.println(response.getBody().user.name);
-//            ObjectMapper mapper = new ObjectMapper();
-//            AuthResponse authResponse = mapper.readValue(response.getBody(), AuthResponse.class);
-//            System.out.println(authResponse.success);
-//            System.out.println(authResponse.user);
-        }catch (Exception e){
-            System.out.println("Not Authenticated...");
-            System.out.println(e.getMessage());
+        if(!authentication.verify()){
+            ApiResponse apiResponse = new ApiResponse();
+            apiResponse.setMessage("Not Authenticated");
+            return new ResponseEntity<>(apiResponse.getBodyResponse(), HttpStatus.UNAUTHORIZED);
         }
-
-
 
         RatingsResponse ratings = restTemplate.getForObject("http://ratings-service/ratings/all",RatingsResponse.class);
 
@@ -88,21 +79,4 @@ public class MoviesController {
     }
 }
 
-class AuthResponse{
-//    @JsonProperty("timestamp")
-//    public String timestamp;
-//    @JsonProperty("message")
-//    public String message;
-    @JsonProperty("success")
-    public boolean success;
-    @JsonProperty("user")
-    public User user;
 
-    public AuthResponse() {
-    }
-}
-
-class User{
-    public long id;
-    public String name;
-}
---
+++ MovieService/src/main/java/io/tkouleris/movieservice/service/Authentication.java
@@ -0,0 +1,63 @@
+package io.tkouleris.movieservice.service;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.tkouleris.movieservice.entity.User;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class Authentication {
+
+    private User loggedInUser;
+
+    public boolean  verify()
+    {
+        String token = this.getToken();
+        var entity = this.setHeaders(token);
+        RestTemplate authRestTemplate = new RestTemplate();
+        try {
+            ResponseEntity<AuthResponse> response = authRestTemplate.exchange("http://127.0.0.1:8000/api/verify", HttpMethod.POST, entity,  AuthResponse.class);
+            if(response.getBody() != null){
+                this.loggedInUser = response.getBody().user;
+                return true;
+            }
+            return false;
+
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    private String getToken()
+    {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization");
+    }
+
+    private HttpEntity setHeaders(String token)
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization",token);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity(headers);
+    }
+
+    public User getLoggedInUser()
+    {
+        return this.loggedInUser;
+    }
+}
+
+class AuthResponse{
+    @JsonProperty("success")
+    public boolean success;
+    @JsonProperty("user")
+    public User user;
+
+    public AuthResponse() {
+    }
+}
